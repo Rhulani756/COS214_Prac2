8a8be8d94b1b8d9c798073777597eaf87e2cc7e1
Rhulani <rhulanimatiane756@gmail.com>
2024-08-08 18:50:07 +0200

    Initialisation of project with UML

---
+++ cmake-build-debug/CMakeFiles/Prac2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Prac2.dir/main.cpp.o"
+  "Prac2.pdb"
+  "Prac2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Prac2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
